279c916513a2f3fd8a4eb5a35102e86092bb30b5
Mahadevan Reji <rmahadevan574@gmail.com>
2025-10-21 21:53:06 +0530

    Refactor database connection to a separate module

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const path = require('path');
 const app = express();
 const port = 3000;
 
-// Database connection
-const db = new sqlite3.Database('./library.db', (err) => {
-  if (err) {
-    console.error('Error connecting to database:', err.message);
-  } else {
-    console.log('Connected to the SQLite database');
-  }
-});
+const db = require('./connection');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
